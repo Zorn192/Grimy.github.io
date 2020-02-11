d0fd9f75fc9a48d79f210f6f1b5938c73efc8d31
Zeker0 <zorn@hotmail.co.uk>
2020-02-11 16:55:17 +0000

    Update trimps.ts

--- src/trimps.ts
+++ src/trimps.ts
@@ -162,7 +162,7 @@ function handle_paste(ev: ClipboardEvent, read_save: () => void, main: () => voi
 	try {
 		game = JSON.parse(LZString.decompressFromBase64(save_string));
 		let min_version = 4.9;
-		let max_version = 4.91;
+		let max_version = 5.8;
 		if (game.global.version > max_version + 0.009)
 			show_alert('warning', `This calculator only supports up to v${max_version} of Trimps, but your save is from v${game.global.version}. Results may be inaccurate.`);
 		else if (game.global.version < min_version)
